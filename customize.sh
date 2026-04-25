cd56e694a77440c3be0480335509311cbb828d1d
dedsecproyt <60039962+dedsecproyt@users.noreply.github.com>
2026-04-25 20:33:27 +0300

    Updated Hosts File
    # Total Blocked: 240146 Included BlockLists:
    1. DEDSEC PRO BlockList
    2. AdGuard DNS filter
    3. Dan Pollock's List
    4. Dandelion Sprout's Anti Push Notifications
    5. Dandelion Sprout's Anti-Malware List
    6. IRN: PersianBlocker list
    7. KOR: YousList
    8. MKD: Macedonian Pi-hole Blocklist
    9. NOR: Dandelion Sprouts nordiske filtre
    10. NoCoin Filter List
    11. Phishing Army
    12. SWE: Frellwit's Swedish Hosts File
    13. Stalkerware Indicators List
    14. Steven Black's List
    15. TUR: turk-adlist
    16. The Big List of Hacked Malware Web Sites
    17. VNM: ABPVN List
    18. uBlock₀ filters – Badware risks

--- customize.sh
+++ customize.sh
@@ -3,15 +3,11 @@ HOSTS_DIR="$MODPATH/system/etc"
 TARGET_HOSTS="$HOSTS_DIR/hosts"
 mkdir -p "$HOSTS_DIR"
 if [ -f "$MODPATH/hosts" ]; then
-    ui_print "- Installing ADS Blocked hosts file..."
     mv -f "$MODPATH/hosts" "$TARGET_HOSTS"
-    ui_print "- Setting permissions..."
     set_perm "$TARGET_HOSTS" 0 0 0777
-    ui_print "- ADS Blocker applied successfully."
+    ui_print "- ADS Blocker installed!"
 else
-    ui_print "!"
     ui_print "! ERROR: 'hosts' file not found in module!"
-    ui_print "!"
     abort
 fi
 
